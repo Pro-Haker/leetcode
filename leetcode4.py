65559707646c100301c01d53b154f4f5dc8b8be1
Pro-Haker <julek.wilczek@gmail.com>
2025-02-03 20:05:33 +0100

    revised 4

--- leetcode4.py
+++ leetcode4.py
@@ -2,6 +2,6 @@ class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
         nums = sorted(nums1 + nums2)
         if len(nums) % 2 == 1:
-            return nums[int(len(nums) / 2 - 0.5)]
+            return nums[len(nums) // 2]
         else:
-            return (nums[int(len(nums) / 2)] + nums[int(len(nums) / 2 - 1)]) / 2
+            return (nums[len(nums) // 2] + nums[len(nums) // 2 - 1]) / 2
