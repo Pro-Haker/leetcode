faf294367594198ebb0e884b5281d76262e71b48
Pro-Haker <julek.wilczek@gmail.com>
2025-02-05 20:12:22 +0100

    improved 1790

--- leetcode1790.py
+++ leetcode1790.py
@@ -1,16 +1,13 @@
 class Solution:
     def areAlmostEqual(self, s1, s2):
-        for idx1 in range(len(s1)):
-            for idx2 in range(len(s1)):
-                list1 = list(s1)
-                list2 = list(s2)
-                buffer = list1[idx2]
-                list1[idx2] = list1[idx1]
-                list1[idx1] = buffer
-                buffer = list2[idx2]
-                list2[idx2] = list2[idx1]
-                list2[idx1] = buffer
-                swappeds1 = ''.join(list1)
-                swappeds2 = ''.join(list2)
-                if swappeds1 == s2 or s1 == swappeds2: return True
-        return False
+        if s1 == s2: return True
+        changes = 0
+        idxs = []
+        for idx in range(len(s1)):
+            if s1[idx] != s2[idx]:
+                changes += 1
+                idxs.append(idx)
+        if changes != 2: return False
+        if s1[idxs[0]] != s2[idxs[1]]: return False
+        if s2[idxs[0]] != s1[idxs[1]]: return False
+        return True
